b4f07d3a15e12e0d2cddd1a63136387e3fa109a3
Kristus310 <jay.fury53@gmail.com>
2026-05-09 22:26:58 +0200

    Updated UI

--- apps/game/urls.py
+++ apps/game/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = "game"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("lobby/", views.lobby, name="lobby"),
+    path("game/", views.game, name="game"),
 ]
--- apps/game/views.py
+++ apps/game/views.py
@@ -2,3 +2,16 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "game/index.html")
+
+def lobby(request):
+    return render(request, "game/lobby.html")
+
+def game(request):
+    lyrics = "Far away, this ship is taking me far away. Far away from the ________ of the people who care if I live or die."
+
+    context = {
+        "lyrics": lyrics,
+        "lives": range(3),
+    }
+
+    return render(request, "game/game.html", context=context)
